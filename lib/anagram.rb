423de1a6d39ec609a028c862d9f0221779ead481
Leora Soibelman <21lsoibelman@gannacademy.org>
2018-07-16 01:33:43 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_reader :input 
+  def initialize(input = 'default')
+    @input = input 
+  end 
+def match(word_array)
+  return_array = []
+  word_array.each do |word| 
+    if @input.split("").sort == word.split("").sort
+      return_array << word
+    end 
+  end 
+  return_array
+end 
+end 
